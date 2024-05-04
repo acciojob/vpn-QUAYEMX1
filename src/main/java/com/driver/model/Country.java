ffd82ec4eab36a6e318a7ed52b18dcde8255e30f
Skashi-katore <katoresakshi@gmail.com>
2024-05-04 13:26:33 +0530

    a

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -1,1 +1,44 @@
 // Note: Do not write @Enumerated annotation above CountryName in this model.
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Country{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int CountryId;
+
+    private CountryName countryName;
+    private String countryCode;
+
+    @JoinColumn
+    @OneToOne
+    private User user;
+
+    @JoinColumn
+    @ManyToOne
+    private ServiceProvider serviceProvider;
+
+    public int getCountryId(){return CountryId;}
+
+    public void setCountryId(int CountryId){this.CountryId=CountryId;}
+
+    public CountryName getCountryName(){return countryName;}
+
+    public void setCountryName(CountryName countryName){this.countryName=countryName;}
+
+    public String getCountryCode(){return countryCode;}
+
+    public void setCountryCode(String countryCode){this.countryCode=countryCode;}
+
+    public User getUser(){return user;}
+
+    public void setUser(User user){this.user=user;}
+
+    public ServiceProvider getServiceProvider(){return serviceProvider;}
+
+    public void setServiceProvider(ServiceProvider serviceProvider){this.serviceProvider=serviceProvider;}
+
+}
